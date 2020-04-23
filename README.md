1eba36c47471e93a05b1a2ac285e572fc625649b
Kayo Timoteo Rossales <timoteokayo@gmail.com>
2020-04-22 21:16:17 -0400

    Projeto Finalizado!

---
+++ README.md
@@ -0,0 +1,26 @@
+## Desafio 07 - GoFinances Web
+
+### ✏️Projeto
+
+Desafio proposto pelo Bootcamp GoStack da Rocketseat, para fixar o aprendizado, ReactJS, listando transações, e realizando importações de arquivos direto para o Back-end pela aplicação.
+
+### 🖥Tecnologias usadas
+
+- [ReactJS](https://reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [Cors](https://www.npmjs.com/package/cors)
+- [Jest](https://jestjs.io/)
+
+### 🏃‍♀️Como Executar
+
+Após clonar o repositório, em um Terminal execute os seguintes comandos:
+
+    cd bootcamp-gostack-desafio07
+    yarn
+    yarn start
+
+### 🗒Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](https://github.com/kayotimoteo/bootcamp-gostack-desafio07/blob/master/LICENSE) para mais detalhes.
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance | null>(null);
+
+  useEffect(() => {
+    api.get('transactions').then((response) => {
+      // console.log(response.data.transactions);
+      setTransactions(response.data.transactions);
+    });
+    api.get('transactions').then((response) => {
+      setBalance(response.data.balance);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">
+              {balance && formatValue(balance.income)}
+            </h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">
+              {balance && formatValue(balance.outcome)}
+            </h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">
+              {balance && formatValue(balance.total)}
+            </h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome'
+                      ? `- ${formatValue(transaction.value)}`
+                      : formatValue(transaction.value)}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>20/04/2020</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
